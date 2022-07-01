6fa1c9a4cb67f46e76c8eadad69a52c428346bc0
Bruno <brunodinosantos@outlook.com>
2022-07-01 13:58:23 -0300

    feat: added dependencies

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { useScrollSmooth } from "../hooks/useScrollSmooth";
+import { Home } from "./Home";
 
 export const App: React.FC = () => {
   useScrollSmooth();
@@ -7,9 +8,7 @@ export const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-
         <Route path="/" element={<Home />} />
->
       </Routes>
     </Router>
   );
