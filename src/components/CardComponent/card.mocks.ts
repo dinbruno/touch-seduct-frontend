8631ee3215eebc118b7015e3a00a5ef010535e78
Bruno <brunodinosantos@outlook.com>
2022-07-04 18:13:36 -0300

    feat: mocks of card components

---
+++ src/components/CardComponent/card.mocks.ts
@@ -0,0 +1,107 @@
+export const CARDMOCKS = [
+    {
+        id: 1,
+        cod: "A1",
+        avaible: true,
+        sizes: {
+            "12": "12",
+            "14": "14",
+            "P": "P",
+            "M": "M",
+            "G": "G",
+            "GG": "GG"
+        },
+        colors: {
+            blue: "blue",
+            yellow: "yellow",
+            pink: "Pink"            
+
+        },
+        
+    },
+    {
+        id: 2,
+        cod: "A2",
+        avaible: true,
+        sizes: {
+            "12": "12",
+            "14": "14",
+            "P": "P",
+            "M": "M",
+            "G": "G",
+            "GG": "GG"
+        },
+        colors: {
+            blue: "blue",
+            yellow: "yellow",
+            pink: "Pink"            
+
+        },
+        
+        
+    },
+    {
+        id: 2,
+        cod: "A2",
+        avaible: true,
+        sizes: {
+            "12": "12",
+            "14": "14",
+            "P": "P",
+            "M": "M",
+            "G": "G",
+            "GG": "GG"
+        },
+        colors: {
+            blue: "blue",
+            yellow: "yellow",
+            pink: "Pink"            
+
+        },
+        
+        
+    },
+    {
+        id: 2,
+        cod: "A2",
+        avaible: true,
+        sizes: {
+            "12": "12",
+            "14": "14",
+            "P": "P",
+            "M": "M",
+            "G": "G",
+            "GG": "GG"
+        },
+        colors: {
+            blue: "blue",
+            yellow: "yellow",
+            pink: "Pink"            
+
+        },
+        
+        
+    },
+    {
+        id: 2,
+        cod: "A2",
+        avaible: true,
+        sizes: {
+            "12": "12",
+            "14": "14",
+            "P": "P",
+            "M": "M",
+            "G": "G",
+            "GG": "GG"
+        },
+        colors: {
+            blue: "blue",
+            yellow: "yellow",
+            pink: "Pink"            
+
+        },
+        
+        
+    },
+    
+]
