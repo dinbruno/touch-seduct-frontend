30637dd1585ae5662cd0d55dac61a533ac2767f6
Bruno <brunodinosantos@outlook.com>
2022-07-05 12:05:46 -0300

    feat: select products details

--- src/pages/Home/Components/SelectProducts/selectProducts.styles.ts
+++ src/pages/Home/Components/SelectProducts/selectProducts.styles.ts
@@ -1,5 +1,79 @@
+import Bdsm from "../../../../assets/png/bdsm.png";
 import styled from "styled-components";
 
+const Container = styled.div`
+  width: calc(100% - 15rem);
+  height: 250px;
+  margin: 0 auto;
+  margin-top: -10rem;
+  border-bottom: #f1b 1px solid;
+  border-radius: 10px;
+  background-color: ${({ theme }) => theme.theme.neonShader};
+  box-shadow: inset 0 0 12px 0 #f1b, 0 0 52px 0 #f1b;
+  text-shadow: 0 0 8px hsl(0 0% 100% / 0.3), 0 0 32px currentColor;
+  display: flex;
+  /* background-image: url(${Bdsm}); */
+  background-size: cover;
+  z-index: 10;
+`;
+
+const Section = styled.div`
+  width: calc(100% - 2rem);
+  height: 180px;
+  background-color: transparent;
+  margin: 0 auto;
+  margin-top: -50px;
+  border-radius: 15px;
+`;
+
+const Ul = styled.ul`
+  display: flex;
+  gap: 30px;
+  justify-content: initial;
+  align-items: center;
+  list-style-type: none;
+  
+  > li {
+    border: 1px solid #f1b;
+    padding: 40px;
+    border-radius: 5px;
+    font-size: 22px;
+  }
+  li:nth-child(1) {
+    :hover {
+      color: white;
+      border: 1px solid white;
+
+  
+    }
+  }
+  li:nth-child(3) {
+    :hover {
+      color: #84b2e5;
+      border: 1px solid #84b2e5;
+    }
+  }
+  li:nth-child(2) {
+    :hover {
+      color: red;
+      border: 1px solid red;
+    }
+  }
+  li:nth-child(4) {
+    :hover {
+      color: #00D28F;
+      border: 1px solid #00D28F;
+    }
+  }
+`;
+
+const ImageContainer = styled.div`
+  margin-top: 13rem;
+`
+
 export const Sty = {
-    
-}
+  Container,
+  Section,
+  Ul,
+  ImageContainer
+};
--- src/pages/Home/Components/SelectProducts/selectProducts.tsx
+++ src/pages/Home/Components/SelectProducts/selectProducts.tsx
@@ -1,5 +1,25 @@
+import { useState } from "react"
+import { Sty } from "."
+import Bdsm from "../../../../assets/png/bdsm.png"
+import { CardComponent } from "../../../../components/CardComponent"
 
 
-export const selectProducts: React.FC = () => {
-    return <></>
+export const SelectProducts: React.FC = () => {
+    const [selected, setSelected] = useState<number>(0)
+
+    return( 
+    <Sty.Container>
+        <Sty.Section>
+            <Sty.Ul>
+                <li onClick={() => setSelected(1)}>Lingeries</li>
+                <li onClick={() => setSelected(2)}>BDSM</li>
+                <li onClick={() => setSelected(3)}>Cosméticos</li>
+                <li onClick={() => setSelected(4)}>Brinquedos</li>
+            </Sty.Ul>
+            <Sty.ImageContainer>
+            {/* <CardComponent/> */}
+            </Sty.ImageContainer>
+        </Sty.Section>
+    </Sty.Container>
+    )
 }
