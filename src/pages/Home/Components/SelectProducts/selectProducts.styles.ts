71ee3798b7e293c390a8d01df4823051a585bf27
Bruno <brunodinosantos@outlook.com>
2022-07-06 18:03:05 -0300

    feat: select products formatted

--- src/pages/Home/Components/SelectProducts/selectProducts.styles.ts
+++ src/pages/Home/Components/SelectProducts/selectProducts.styles.ts
@@ -1,79 +1,95 @@
 import Bdsm from "../../../../assets/png/bdsm.png";
 import styled from "styled-components";
 
-const Container = styled.div`
-  width: calc(100% - 15rem);
-  height: 250px;
-  margin: 0 auto;
-  margin-top: -10rem;
-  border-bottom: #f1b 1px solid;
-  border-radius: 10px;
-  background-color: ${({ theme }) => theme.theme.neonShader};
-  box-shadow: inset 0 0 12px 0 #f1b, 0 0 52px 0 #f1b;
-  text-shadow: 0 0 8px hsl(0 0% 100% / 0.3), 0 0 32px currentColor;
+
+const ContainerFull = styled.div`
+  width: 100%;
+  height: 100%;
+
+`
+
+const SectionComponentContent = styled.div`
+  
+`
+
+const ContainerSelect = styled.div`
   display: flex;
-  /* background-image: url(${Bdsm}); */
-  background-size: cover;
-  z-index: 10;
+  align-items: center;
+  justify-content: flex-end;
 `;
 
 const Section = styled.div`
-  width: calc(100% - 2rem);
-  height: 180px;
-  background-color: transparent;
+   width: 800px;
+  height: 250px;
   margin: 0 auto;
-  margin-top: -50px;
-  border-radius: 15px;
+  border-bottom: #f1b 1px solid;
+  border-radius: 10px;
+  /* background-color: ${({ theme }) => theme.theme.neonShader};
+  box-shadow: inset 0 0 12px 0 #f1b, 0 0 52px 0 #f1b;
+  text-shadow: 0 0 8px hsl(0 0% 100% / 0.3), 0 0 32px currentColor; */
+  display: flex;
+  z-index: 10;
 `;
 
 const Ul = styled.ul`
   display: flex;
   gap: 30px;
-  justify-content: initial;
+  justify-content: center;
   align-items: center;
   list-style-type: none;
-  
+
   > li {
     border: 1px solid #f1b;
     padding: 40px;
     border-radius: 5px;
     font-size: 22px;
+    transition: 0.2s ease-in-out;
   }
   li:nth-child(1) {
     :hover {
+      padding: 60px;
       color: white;
       border: 1px solid white;
-
-  
     }
   }
   li:nth-child(3) {
     :hover {
+      padding: 60px;
+
       color: #84b2e5;
       border: 1px solid #84b2e5;
     }
   }
   li:nth-child(2) {
     :hover {
+      padding: 60px;
+
       color: red;
       border: 1px solid red;
     }
   }
   li:nth-child(4) {
     :hover {
-      color: #00D28F;
-      border: 1px solid #00D28F;
+      padding: 60px;
+
+      color: #00d28f;
+      border: 1px solid #00d28f;
     }
   }
 `;
 
 const ImageContainer = styled.div`
   margin-top: 13rem;
-`
+`;
+
+const SelectProdutosContent = styled.div``
 
 export const Sty = {
-  Container,
+  ContainerFull,
+  ContainerSelect,
   Section,
   Ul,
-  ImageContainer
+  ImageContainer,
+  SelectProdutosContent,
+  SectionComponentContent
 };
--- src/pages/Home/Components/SelectProducts/selectProducts.tsx
+++ src/pages/Home/Components/SelectProducts/selectProducts.tsx
@@ -1,25 +1,34 @@
-import { useState } from "react"
-import { Sty } from "."
-import Bdsm from "../../../../assets/png/bdsm.png"
-import { CardComponent } from "../../../../components/CardComponent"
-
+import { useState } from "react";
+import { Sty } from ".";
+import { CardComponent } from "../../../../components/CardComponent";
+import { SectionComponent } from "../SectionComponent";
 
 export const SelectProducts: React.FC = () => {
-    const [selected, setSelected] = useState<number>(0)
+  const [selected, setSelected] = useState<number>(0);
 
-    return( 
-    <Sty.Container>
+  return (
+    <Sty.ContainerFull>
+      <Sty.SectionComponentContent>
+          {/* Componente do painel brilhante */}
+        <SectionComponent />
+      </Sty.SectionComponentContent>
+
+      <Sty.ContainerSelect>
         <Sty.Section>
-            <Sty.Ul>
-                <li onClick={() => setSelected(1)}>Lingeries</li>
-                <li onClick={() => setSelected(2)}>BDSM</li>
-                <li onClick={() => setSelected(3)}>Cosméticos</li>
-                <li onClick={() => setSelected(4)}>Brinquedos</li>
-            </Sty.Ul>
-            <Sty.ImageContainer>
-            {/* <CardComponent/> */}
-            </Sty.ImageContainer>
+          <Sty.Ul>
+            <li onClick={() => setSelected(1)}>Lingeries</li>
+            <li onClick={() => setSelected(2)}>BDSM</li>
+            <li onClick={() => setSelected(3)}>Cosméticos</li>
+            <li onClick={() => setSelected(4)}>Brinquedos</li>
+          </Sty.Ul>
+          <Sty.ImageContainer></Sty.ImageContainer>
         </Sty.Section>
-    </Sty.Container>
-    )
-}
+      </Sty.ContainerSelect>
+      
+      <Sty.SelectProdutosContent>
+        {/* Componente que contém os cards dos produtos */}
+        <CardComponent />
+      </Sty.SelectProdutosContent>
+    </Sty.ContainerFull>
+  );
+};
