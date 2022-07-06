9779349542d53e6c6ebc9c02aff976263d515561
Bruno <brunodinosantos@outlook.com>
2022-07-06 18:04:10 -0300

    feat: data mock

---
+++ src/pages/Home/Components/FrequentlyDoubts/Data.tsx
@@ -0,0 +1,22 @@
+export const QUESTIONS = [
+  {
+    id: 1,
+    question: "Como as compras são realizadas?",
+    answer: "São realizadas via whatsapp",
+  },
+  {
+    id: 2,
+    question: "Como são feitas as entregas?",
+    answer: "Podem ser feitas através do correio (frete por sua conta) ou pelas estações do metrô !",
+  },
+  {
+    id: 3,
+    question: "Quais os métodos de pagamento?",
+    answer: "Aceitamos PIX, PicPay, geramos boleto e QR code paga pagamento",
+  },
+  {
+    id: 4,
+    question: "qFour",
+    answer: "aFour",
+  },
+]
