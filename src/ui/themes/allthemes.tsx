3cd283e4352b53b0d332dab035810e88715fe41b
Bruno <brunodinosantos@outlook.com>
2022-07-09 17:01:59 -0300

    fix: theme

--- src/ui/themes/allthemes.tsx
+++ src/ui/themes/allthemes.tsx
@@ -5,26 +5,25 @@ export const darkTheme = {
 
     bg: "#C5C5C5",
     shape: "#08090A",
-    
+
     faq: "#494949",
 
     neonShader: "#08090A",
-    footer: "#08070B"
+    footer: "#08070B",
   },
 };
 
 export const lightTheme = {
-  theme: {                                                                                          
+  theme: {
     paragraph: "#967AA1",
     font: "#E8D7F1",
 
     bg: "#F5E6E8",
     shape: "#967AA1",
 
-    faq:"#D1B1C8",
+    faq: "#D1B1C8",
 
     neonShader: "#A14581",
-    footer: "#323B70"
-
+    footer: "#323B70",
   },
 };
